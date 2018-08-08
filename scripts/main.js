fcb6c3fa77b86965fe023ec90a3de099d48c804a
Johann Wentzel <wentzel142@gmail.com>
2018-08-08 16:15:18 -0600

    Removed random alert

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,6 @@
      document.getElementById("typeString").innerHTML = "Clicked!";
  }
 
- $( document ).ready(function() {
-    alert("Hi");
-    console.log("Hello");
-});
-
 var instance = new TypeIt('#typeString', {
     strings: ["mobile developer.", "game developer.", "web developer.", "web designer.",],
     //-- Other options...
